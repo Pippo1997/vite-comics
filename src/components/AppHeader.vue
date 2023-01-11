f99f5dd5848196e75b4227c184189a802d0b7551
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-01-11 19:05:55 +0100

    debug img

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,76 @@
 <script>
 export default {
-  
+    data() {
+        return {
+            menu: [
+                {
+                    label: 'Characters',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Comics',
+                    url: '#',
+                    active: true
+                },
+                {
+                    label: 'Movies',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'TV',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Games',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Video',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Fans',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'News',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Shop',
+                    url: '#',
+                    active: false
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <template lang="">
   <header>
-    ciao
+    <a href="/" class="logo">
+        <img src="../assets/img/dc-logo.png" alt="logo-dc">
+    </a>
+    <nav>
+        <ul>
+            <li v-for="(item, index) in menu" :key="index">
+                <a :href="item.url" :class="item.active ? 'active' : ''">
+                {{item.label}}
+                </a>
+            </li>
+        </ul>
+       </nav>
   </header>
 </template>
 
-<style lang="">
+<style lang="scss">
   
 </style>
